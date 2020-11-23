954fc6d2238679850407d6dd52d8dc1a00893d49
Rami Ruotsalainen <Rami.Ruotsalainen@student.lab.fi>
2020-11-23 12:57:07 +0200

    Add project files.

---
+++ Applicatiobn/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+using System;
+
+public class Survey
+{
+	private string surveyTitle;
+	private string status;
+	private DateTime closedt;
+	private DateTime startdt;
+
+	public string SetSurvey();
+	
+
+	public string SetQuestion()
+
+
+
+
+
+}
+
+public class Manager
+{
+	private int mID;
+	private string mName;
+	private int managerKey;
+
+	public string SetmName()
+
+	public int GenerateKey()
+
+	public void GetmKey()
+}
+
+public class Database
+{
+
+}
+
+public class Application
+{
+
+}
+
+public class Admin
+{
+
+}
+
+public class managerKey
+{
+
+}
+
+public class respodentKey
+{
+
+}
+
+public class Page
+{
+
+}
+
+public class Question
+{
+
+}
+
+public class Response
+{
+
+}
+
+
+namespace Applicatiobn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
